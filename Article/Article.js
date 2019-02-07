4060f22293e1009ba118f13716d9ffef3f98c16c
Talent Antonio <antonio.talenty@gmail.com>
2019-02-07 01:04:38 +0000

    Added Expand and Close button but not yet working

--- Article/Article.js
+++ Article/Article.js
@@ -3,21 +3,34 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton.textcontent = 'expand';
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click)', this.expandArticle.bind(this));
+
+
+    this.closeButton = this.domElement.querySelector('.closeButton');
+    this.closeButton.textcontent = 'Close';
+    this.closeButton.addEventListener('click)', this.closeArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    for(let i = 0; i< articles.length; i++){
+      console.log(articles[i]);
+      articles[i].classList.remove('article-open');
+    }
 
+     this.domElement.classList.toggle('article-open');
+  }
+  closeArticle(){
+    this.domElement.classList.remove('article-open');
   }
 }
-
 /* START HERE: 
 
 - Select all classes named ".article" and assign that value to the articles variable.  
@@ -26,4 +39,10 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+    articles.forEach(article => {
+      return new Article(article);
+    })
+
+    // let btn =  document.querySelector(".expandButton");
+    // btn.textContent = siteContent["article"]["expandButton"];
